22f83dc51f4162dcdf8418edc43a55a44963a00c
felix-atoma <felixatoma2@gmail.com>
2025-06-07 14:04:58 +0000

    added api rout

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+// This file sets up an Axios instance for making API requests
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+});
+
+// Add request interceptor for auth token
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export const createOrder = (orderData) => api.post('/orders', orderData);
+export const getOrder = (id) => api.get(`/orders/${id}`);
+
+export default api;
